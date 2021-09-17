4b4acb6eba2a786bdc6280d52824f0ac684592b1
hermione <hermione-legal@outlook.com>
2021-09-18 02:39:36 +0500

    refactor common functions

--- microservice/src/common.service.ts
+++ microservice/src/common.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from "@nestjs/common";
 
+// Методы в этом классе переписывать нельзя - они имитируют внешние небезопасне функции
+
+export class CommonServiceError extends Error {
+  name = 'CommonServiceError'
+}
+
 @Injectable()
 export class CommonService {
-  async throwablePromise(): Promise<number> {
-    console.log('throw')
-    throw new Error('Throw promise')
-    return 1
+  async throwablePromise(error = false): Promise<string> {
+    if (error) {
+      throw new Error('Throw promise')
+    }
+    return "Sir"
   }
 
-  async throwablePromiseConstructor(): Promise<number> {
+  async throwablePromiseConstructor(error = false): Promise<number> {
     return new Promise((resolve, reject) => {
       reject('Reject promise')
       return 1;
     })
+  }
+
+  someFunctionThatReturnsError(error = false) {
+    if (error) {
+      throw new CommonServiceError("Error for test")
+    }
+    else {
+      return "Sir"
+    }
   }
 
   executeCallback(callback) {
